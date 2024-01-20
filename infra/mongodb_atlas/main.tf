d1688bd934128e714f9dc13377f2667b6b43b0ce
jyo <jyo.liar@gmail.com>
2024-01-20 19:20:07 +0900

    private_endpoint의 url output 추가

--- infra/mongodb_atlas/main.tf
+++ infra/mongodb_atlas/main.tf
@@ -1,8 +1,3 @@
-
-# provider "mongodbatlas" {
-#   public_key  = var.atlas_key.public_key
-#   private_key = var.atlas_key.private_key
-# }
 terraform {
   required_providers {
     mongodbatlas = {
@@ -96,12 +91,15 @@ resource "aws_vpc_endpoint" "vpc_endpoint" {
   vpc_id              = var.vpc_id
   service_name        = each.value.endpoint_service_name
   vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+  private_dns_enabled = false
 
   security_group_ids = [var.mongodb_sg_id]
   subnet_ids         = var.subnet_ids
 }
 
+output "private_endpoint" {
+  value = [for mongodb in mongodbatlas_serverless_instance.mongodb_serverless : mongodb.connection_strings_private_endpoint_srv[0]]
+}
 # resource "mongodbatlas_privatelink_endpoint_service_serverless" "privatelink_endpoint_service" {
 #   for_each = mongodbatlas_privatelink_endpoint_serverless.privatelink_endpoint
 
