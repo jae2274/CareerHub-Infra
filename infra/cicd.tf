89241777ce7b0af7d96ad8dee64aef52e4149f02
jyo <jyo.liar@gmail.com>
2024-02-05 14:06:32 +0900

    logApi cicd 인프라 추가

--- infra/cicd.tf
+++ infra/cicd.tf
@@ -27,6 +27,19 @@ module "dataprocessor_cicd" {
   subnet_arns     = [for subnet in local.private_subnets : subnet.arn]
 }
 
+module "logapi_cicd" {
+  source = "./cicd_infra"
+
+  other_latest_tag = local.other_latest_tag
+  cicd_name        = "${local.prefix_service_name}-logapi"
+
+  repository_path = "jae2274/LogApi"
+  branch_name     = local.branch
+  vpc_id          = local.vpc_id
+  subnet_ids      = [for subnet in local.private_subnets : subnet.id]
+  subnet_arns     = [for subnet in local.private_subnets : subnet.arn]
+}
+
 locals {
   dataprovider_ecr = {
     region = module.dataprovider_cicd.ecr_region
