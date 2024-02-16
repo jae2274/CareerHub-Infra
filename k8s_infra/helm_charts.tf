004db0fb3b2b9c4b29ed79ac7f2506568a2bbd21
jyo <jyo.liar@gmail.com>
2024-02-16 16:54:40 +0900

    values_template.yaml 파일을 각 차트에 맞게 분리

--- k8s_infra/helm_charts.tf
+++ k8s_infra/helm_charts.tf
@@ -1,3 +1,12 @@
+locals {
+  charts = {
+    log_api = {
+      image     = local.logapi_ecr
+      mongo_uri = local.log_mongodb_endpoint
+    }
+  }
+}
+
 data "external" "helm_charts" {
   program = ["bash", "${path.module}/data_external/helm_charts.sh", "helm"]
 }
@@ -8,8 +17,7 @@ resource "local_file" "value_yaml" {
   for_each = toset(split(",", data.external.helm_charts.result.list))
 
   filename = "${path.module}/${each.key}values.yaml"
-  content = templatefile("${path.module}/helm/values_template.yaml", {
-    logapi_image     = local.logapi_ecr
-    logapi_mongo_uri = local.log_mongodb_endpoint
+  content = templatefile("${path.module}/${each.key}values_template.yaml", {
+    charts = local.charts
   })
 }
