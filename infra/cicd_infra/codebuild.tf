0d62ad08ce70966bd625a0f0e891c2c4baeda0de
jyo <jyo.liar@gmail.com>
2024-02-18 15:10:01 +0900

    codebuild의 인스턴스를 arm에서 amd로 변경

--- infra/cicd_infra/codebuild.tf
+++ infra/cicd_infra/codebuild.tf
@@ -130,9 +130,9 @@ resource "aws_codebuild_project" "codebuild_project" {
 
   environment {
     image_pull_credentials_type = "CODEBUILD"
-    type                        = "ARM_CONTAINER"
+    type                        = "LINUX_CONTAINER"
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-aarch64-standard:3.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     privileged_mode             = true
   }
 
