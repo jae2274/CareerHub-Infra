59b81488a4faa23b5ec5a5d26c2506ebb2984db9
jyo <jyo.liar@gmail.com>
2024-04-02 18:32:46 +0900

    ecr 인프라 제거시 내부 image도 강제 제거

--- helm_infra/helm_repo_infra/main.tf
+++ helm_infra/helm_repo_infra/main.tf
@@ -27,7 +27,7 @@ resource "null_resource" "chart_package" {
 resource "aws_ecr_repository" "helm_repo" {
   name = local.chart_name //TODO: 반드시! 환경별로 다른 이름을 사용 필요. 현재 구조로는 같은 이름만 사용 가능. 구조 변경 필요.
 
-
+  force_delete = true
   image_scanning_configuration {
     scan_on_push = true
   }
--- infra/backend_cicd_infra/ecr.tf
+++ infra/backend_cicd_infra/ecr.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "ecr_repo" {
   name = "${var.cicd_name}-ecr"
 
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 data "aws_region" "current" {}
