45259f32705f7bdfb82d317929874fc79e0ac55a
jyo liar <jyo-liar@gmail.com>
2024-09-30 15:39:17 +0900

    api gateway 호출 제한 설정

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -1,6 +1,21 @@
 locals {
-  api_gateway_name  = "${local.prefix_service_name}-gateway"
-  auth_service_path = "/auth"
+  api_gateway_name = "${local.prefix_service_name}-gateway"
+
+  root_path               = "/"
+  root_proxy_path         = "/{proxy+}"
+  backend_root_proxy_path = "${local.backend_root_path}/{proxy+}"
+  auth_service_path       = "/auth"
+  auth_service_proxy_path = "${local.auth_service_path}/{proxy+}"
+
+  ALL_METHODS = toset([
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE",
+    "PATCH",
+    "HEAD",
+    "OPTIONS",
+  ])
 }
 resource "aws_api_gateway_rest_api" "rest_api_gateway" {
   name = local.api_gateway_name
@@ -13,7 +28,7 @@ resource "aws_api_gateway_rest_api" "rest_api_gateway" {
     }
 
     paths = {
-      "/" = {
+      "${local.root_path}" = {
         get = {
           x-amazon-apigateway-integration = {
             httpMethod           = "ANY"
@@ -23,7 +38,7 @@ resource "aws_api_gateway_rest_api" "rest_api_gateway" {
           }
         }
       }
-      "/{proxy+}" = {
+      "${local.root_proxy_path}" = {
         x-amazon-apigateway-any-method = {
           x-amazon-apigateway-integration = {
             httpMethod           = "ANY"
@@ -44,7 +59,7 @@ resource "aws_api_gateway_rest_api" "rest_api_gateway" {
           ]
         }
       }
-      "${local.backend_root_path}/{proxy+}" = {
+      "${local.backend_root_proxy_path}" = {
         x-amazon-apigateway-any-method = {
           x-amazon-apigateway-integration = {
             httpMethod           = "ANY"
@@ -76,7 +91,7 @@ resource "aws_api_gateway_rest_api" "rest_api_gateway" {
           }
         }
       }
-      "${local.auth_service_path}/{proxy+}" = {
+      "${local.auth_service_proxy_path}" = {
         x-amazon-apigateway-any-method = {
           x-amazon-apigateway-integration = {
             httpMethod           = "ANY"
@@ -118,7 +133,66 @@ resource "aws_api_gateway_stage" "prod_stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api_gateway.id
   stage_name    = "prod"
+
+
 }
+
+resource "aws_api_gateway_method_settings" "root_path_specific" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
+  stage_name  = aws_api_gateway_stage.prod_stage.stage_name
+  method_path = "*/*"
+
+  settings {
+    throttling_rate_limit  = 1
+    throttling_burst_limit = 1
+  }
+}
+
+resource "aws_api_gateway_method_settings" "api_path_specific" {
+  for_each = local.ALL_METHODS
+
+  rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
+  stage_name  = aws_api_gateway_stage.prod_stage.stage_name
+  method_path = "${trimprefix(local.backend_root_proxy_path, "/")}/${each.key}"
+
+  settings {
+    throttling_rate_limit  = 5
+    throttling_burst_limit = 3
+  }
+}
+resource "aws_api_gateway_method_settings" "auth_path_specific" {
+  for_each = local.ALL_METHODS
+
+  rest_api_id = aws_api_gateway_rest_api.rest_api_gateway.id
+  stage_name  = aws_api_gateway_stage.prod_stage.stage_name
+  method_path = "${trimprefix(local.auth_service_proxy_path, "/")}/${each.key}"
+
+  settings {
+    throttling_rate_limit  = 5
+    throttling_burst_limit = 3
+  }
+}
+# resource "aws_api_gateway_usage_plan" "usage_plan" {
+#   name        = "${local.prefix_service_name}-plan"
+
+#   description = "limits the number of requests that can be made to the API"
+
+#   api_stages {
+#     api_id = aws_api_gateway_rest_api.rest_api_gateway.id
+#     stage  = aws_api_gateway_stage.prod_stage.stage_name
+#   }
+
+#   quota_settings {
+#     limit  = 5
+#     offset = 0
+#     period = "DAY"
+#   }
+
+#   throttle_settings {
+#     burst_limit = 1
+#     rate_limit  = 1
+#   }
+# }
 
 # resource "aws_api_gateway_domain_name" "api_gateway_domain_name" {
 #   certificate_arn = aws_acm_certificate.acm_certificate.arn
