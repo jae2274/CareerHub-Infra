fe0b8bfe6589d2922292155366c52dc389fdcfd2
jyo liar <jyo-liar@gmail.com>
2025-01-10 13:51:49 +0900

    ansible playbook 작성 진행중

--- k8s_cluster_infra/terraform_config.tf
+++ k8s_cluster_infra/terraform_config.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/local"
       version = "2.5.1"
     }
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.3.0"
+    }
   }
 }
 
@@ -37,4 +41,4 @@ provider "aws" {
 }
 
 
-
+provider "ansible" {}
