60edc9d918574f839dd87c3c8d60890b5a1f8767
jyo liar <jyo-liar@gmail.com>
2025-01-26 15:22:41 +0900

    k8s_infra 불필요 스크립트 제거

--- k8s_cluster_infra/k8s_infra/cluster/variables.tf
+++ k8s_cluster_infra/k8s_infra/cluster/variables.tf
@@ -40,21 +40,6 @@ data "aws_region" "current" {}
 locals {
   region = data.aws_region.current.name
 
-  install_k8s_sh = file("${path.module}/../init_scripts/install_k8s.sh")
-
-  init_k8s_sh = templatefile("${path.module}/init_scripts/init_k8s.sh", {
-    public_ip = aws_eip.master_public_ip.public_ip
-    ecrs      = var.ecrs #TODO: 불필요한 정보 제거
-  })
-
-  login_ecr_sh = templatefile("${path.module}/init_scripts/login_ecr.sh", {
-    ecrs = var.ecrs
-  })
-
-  set_secret_sh = templatefile("${path.module}/init_scripts/set_secret.sh", {
-    secret_id = aws_secretsmanager_secret.kubeconfig.id
-    master_ip = aws_eip.master_public_ip.public_ip
-  })
   # ami = "ami-0a7cf821b91bcccbc" # ubuntu 20.04 LTS x86_64
   # ami = "ami-025a235c91853ccbe" # ubuntu 20.04 LTS arm64
 }
